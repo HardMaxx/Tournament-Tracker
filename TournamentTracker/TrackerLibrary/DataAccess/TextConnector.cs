524ec9df0202070f962133980c90329ab3c8c266
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-06-05 21:45:15 +0200

    Text connection, write data in to text file

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
         //TODO - Make the CreatePrize method actually save to the text file
         /// <summary>
         /// Saves a new prize to the text file
@@ -15,7 +19,25 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            //load the text file and convert the text to the list>prizemodel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePatch().LoadFile().ConvertToPrizeModels();
+
+            //find the max id
+            int currentId = 1;
+
+            if (prizes.Count>0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            //add the new record with the new id(max+1)
+            prizes.Add(model);
+
+            //conver the prizes to list<sting>
+            //save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
