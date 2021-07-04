bca6e052d2b613168e2fd43022083388aa8440b5
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-07-04 14:19:59 +0200

    Save matchups and matchupEntries in sql

--- TournamentTracker/TrackerLibrary/Models/MatchupEntryModel.cs
+++ TournamentTracker/TrackerLibrary/Models/MatchupEntryModel.cs
@@ -12,6 +12,11 @@ namespace TrackerLibrary.Models
     public class MatchupEntryModel
     {
         /// <summary>
+        /// The unique identifier for the matchup entry..
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
         /// Represent one team in the matchup.
         /// </summary>
         /// 
